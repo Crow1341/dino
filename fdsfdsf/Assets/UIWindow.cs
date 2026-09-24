0d69e6b2ea4c3413d7f950bcfd6fbeece730d788
Crow1341 <cdmx2984@amerike.edu.mx>
2026-09-23 20:44:12 -0600

    aswawrefrfwe

--- fdsfdsf/Assets/UIWindow.cs
+++ fdsfdsf/Assets/UIWindow.cs
@@ -7,6 +7,11 @@ public class UIWindow : MonoBehaviour
     [SerializeField] private RectTransform _canvasRectTransform;
     [SerializeField] private CanvasGroup _canvasGroup;
     [SerializeField] private bool _hideOnStart;
+    [Header("Animation Settings")]
+[SerializeField] private float showDuration = 0.5f;
+[SerializeField] private float hideDuration = 0.5f;
+[SerializeField] private Ease showEase = Ease.OutBack;
+[SerializeField] private Ease hideEase = Ease.InBack;
     void Start()
     {
         Initalize();
